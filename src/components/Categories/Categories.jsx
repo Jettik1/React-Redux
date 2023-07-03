3d96577a25643858defb61bd2b4e61ad3a4265ba
Jettik1 <contrakt120@gmail.com>
2023-07-03 08:38:18 +0300

    Added a categories component

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../../styles/Categories.module.css"
+import {Link} from "react-router-dom";
+
+const Categories = ({ title , products = [], amount }) => {
+    const list = products.filter((_, i) => amount)
+
+    return (
+        <section className={styles.section}>
+            <h2>{title}</h2>
+            <div className={styles.list}>
+                {list.map(({ id, name, image}) => (
+                    <Link to={`/categoties/${id}`} key={id} className={styles.item}>
+                        <div
+                            className={styles.image}
+                            style={{ background: `url(${image})`}}
+                        />
+                        <h3 className={styles.title}>{name}</h3>
+                    </Link>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default Categories;
+
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Poster from "../Poster/Poster";
 import Products from "../Products/Products";
 import {useSelector} from "react-redux";
+import Categories from "../Categories/Categories";
 export const Home = () => {
-    const { list } = useSelector(({ products }) => products)
+    const { products, categories } = useSelector(( state ) => state)
 
     return (
         <>
             <Poster/>
-            <Products products={list} amount={5} title={`ProductsTitle/Home`}/>
+            <Products products={products.list} amount={5} title={`ProductsTitle/Home`}/>
+            <Categories products={categories.list} amount={5} title={`CategoriesTitle/Home`}/>
         </>
     )
 }
